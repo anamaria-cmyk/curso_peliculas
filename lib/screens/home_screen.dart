bca8f5ff4ad97c39c71d55865dbac071b21136fe
anamaria-cmyk <anamariasolorzano013@gmail.com>
2023-08-11 07:58:45 -0500

    segundo repositorio

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Peliculas en cines'),
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {}, 
+            icon: const Icon( Icons.search_outlined ),
+          )
+        ],
+      ), 
+      body: Column(
+        children: [
+          CardSwiper()
+        //listado horizontal de peliculas 
+        
+        ],
+      )
+    );
+  }
+}
